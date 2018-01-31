a4c4a56245abe48a8230aa1ad63829a0369432ca
Alex <regee_roller_66@hotmail.com>
2018-01-31 20:45:48 +0100

    Update and Delete

--- MongoDb/controllers/fruit.js
+++ MongoDb/controllers/fruit.js
@@ -8,43 +8,6 @@ function testController(req, res){
     });
 }
 
-function saveFruit(req, res){
-    var fruit = new Fruit();
-
-    var params = req.body;
-
-    if (params.name) {
-        fruit.name      = params.name;
-        fruit.color     = params.color;
-        fruit.season    = params.season;
-        fruit.available = params.available;
-
-        fruit.save(( err, fruitStored) => {
-            if (err){
-                res.status(500).send({
-                    message: 'Server Error'
-                });
-            }else{
-                if (fruitStored) {
-                    res.status(200).send({
-                       fruit: fruitStored
-                    });
-                }else{
-                    res.status(200).send({
-                        message: 'Fruit was not saved'
-                    });
-                }                
-            }
-
-        });
-    }else{
-        res.status(200).send({
-            message: 'Name of fruit is required'
-        });
-    }
-    
-}
-
 function getFruits(req, res){
     Fruit.find({}).sort({'_id': -1}).exec((err, fruits) => {
         if (err){
@@ -85,13 +48,99 @@ function getFruitById(req, res){
                 });
             }  
         }  
-        
     });
+}
+
+
+function saveFruit(req, res){
+    var fruit = new Fruit();
+
+    var params = req.body;
+
+    if (params.name) {
+        fruit.name      = params.name;
+        fruit.color     = params.color;
+        fruit.season    = params.season;
+        fruit.available = params.available;
+
+        fruit.save(( err, fruitStored) => {
+            if (err){
+                res.status(500).send({
+                    message: 'Server Error'
+                });
+            }else{
+                if (fruitStored) {
+                    res.status(200).send({
+                       fruit: fruitStored
+                    });
+                }else{
+                    res.status(200).send({
+                        message: 'Fruit was not saved'
+                    });
+                }                
+            }
+        });
+    }else{
+        res.status(200).send({
+            message: 'Name of fruit is required'
+        });
+    }
+}
+
+function updateFruit(req, res){
+
+    var fruitId = req.params.id;
+    var update  = req.body;
+
+    Fruit.findByIdAndUpdate( fruitId, update, { new: true }, (err, fruitUpdated) => {
+        if (err){
+            res.status(500).send({
+                message: 'Server Error'
+            });
+        }else{
+            if (fruitUpdated) {
+                res.status(200).send({
+                    fruit: fruitUpdated
+                });
+            }else{
+                res.status(404).send({
+                    message: 'Fruit was not found'
+                });
+            }  
+        }  
+    });
+}
+
+function deletFruit(req, res){
+
+    var fruitId = req.params.id;
+    var update  = req.body; 
+
+    Fruit.findByIdAndRemove(fruitId, (err, FruitRemoved) => {
+        if (err){
+            res.status(500).send({
+                message: 'Server Error'
+            });
+        }else{
+            if (FruitRemoved) {
+                res.status(200).send({
+                    fruit: FruitRemoved
+                });
+            }else{
+                res.status(404).send({
+                    message: 'Fruit was not found'
+                });
+            }  
+        }  
+    });
+
 }
 
 module.exports = {
     testController,
     saveFruit,
     getFruits,
-    getFruitById
+    getFruitById,
+    updateFruit,
+    deletFruit
 };
--- MongoDb/routes/fruit.js
+++ MongoDb/routes/fruit.js
@@ -7,8 +7,10 @@ var FruitController = require('../controllers/fruit');
 var api = express.Router();
 
 api.get('/testing', FruitController.testController);
-api.post('/savefruit', FruitController.saveFruit);
 api.get('/fruits', FruitController.getFruits);
 api.get('/fruit/:id', FruitController.getFruitById);
+api.post('/savefruit', FruitController.saveFruit);
+api.put('/updatefruit/:id', FruitController.updateFruit);
+api.delete('/deletefruit/:id', FruitController.deletFruit);
 
 module.exports = api;
