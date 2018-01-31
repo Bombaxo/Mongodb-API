6b7fbec8ef20d272359f487c7b870c94216e32db
Alex <regee_roller_66@hotmail.com>
2018-01-31 18:42:11 +0100

    Db Connection

---
+++ MongoDb/index.js
@@ -0,0 +1,11 @@
+'use strict'
+
+var mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect('mongodb://localhost:27017/curse_mongodb')
+                .then(() => {
+                    console.log('Connection to DB successful!!');
+                })
+                .catch( err => console.log(err) );
